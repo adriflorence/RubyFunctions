49cc5c40b65bb1753c23a402b0cf8893226d347c
Pete Whittle <peterwhittle75@gmail.com>
2018-05-15 14:51:49 +0100

    Function 5&6 (divide & length)

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -13,3 +13,11 @@ end
 def multiply(num1, num2)
   return num1 * num2
 end
+
+def divide(num1, num2)
+  return num1 / num2
+end
+
+def length_of_string(string)
+  return string.length
+end
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -24,17 +24,17 @@ class Functions_Practice < MiniTest::Test
     assert_equal( 8, multiply_result )
   end
 
-  # def test_divide()
-  #   divide_result = divide( 10, 2 )
-  #   assert_equal( 5, divide_result )
-  # end
-  #
-  # def test_length_of_string()
-  #   test_string = "A string of length 21"
-  #   length_of_string = length_of_string( test_string )
-  #   assert_equal( 21, length_of_string )
-  # end
-  #
+  def test_divide()
+    divide_result = divide( 10, 2 )
+    assert_equal( 5, divide_result )
+  end
+
+  def test_length_of_string()
+    test_string = "A string of length 21"
+    length_of_string = length_of_string( test_string )
+    assert_equal( 21, length_of_string )
+  end
+
   # def test_join_string()
   #   string_1 = "Mary had a little lamb, "
   #   string_2 = "its fleece was white as snow"
