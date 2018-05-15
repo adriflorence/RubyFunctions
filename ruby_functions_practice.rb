bcc1804b8298284af1c2c260f06c953e051bd6ac
Pete Whittle <peterwhittle75@gmail.com>
2018-05-15 15:05:41 +0100

    Functions 9-11 (FullMonthName)

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,3 +25,38 @@ end
 def join_string(string1, string2)
   return string1 + string2
 end
+
+def add_string_as_number(string1, string2)
+  return string1.to_i + string2.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+
+
+  end
+end
